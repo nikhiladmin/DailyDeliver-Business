a8d94882cefc18e3937147deef4f499565c1dfa8
Nikhil Kumar Singh <50423438+nikhilbcps@users.noreply.github.com>
2020-04-20 12:09:57 +0530

    bug fix and full screen added

--- app/src/main/java/com/daytoday/business/dailydelivery/SplashScreenActivity.java
+++ app/src/main/java/com/daytoday/business/dailydelivery/SplashScreenActivity.java
@@ -16,6 +16,7 @@ public class SplashScreenActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
