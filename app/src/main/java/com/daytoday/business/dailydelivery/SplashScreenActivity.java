9b299f01b83b3634ec438fd84d5a5a74b56413c8
Shuvam Kumar <shuvam1982000@gmail.com>
2020-04-20 10:57:30 +0530

    splash 2nd commit

--- app/src/main/java/com/daytoday/business/dailydelivery/SplashScreenActivity.java
+++ app/src/main/java/com/daytoday/business/dailydelivery/SplashScreenActivity.java
@@ -5,14 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class SplashScreenActivity extends AppCompatActivity {
-    private static int SPLASH_SCREEN_TIME = 300; /*This is the Splash screen time which is 3 seconds*/
+    private static int SPLASH_SCREEN_TIME = 3000; /*This is the Splash screen time which is 3 seconds*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
