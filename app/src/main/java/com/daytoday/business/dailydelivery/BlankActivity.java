1e060d6d3f0b4f6ae9058a8f7d85ec5536fa9ce1
Nikhil Kumar Singh <50423438+nikhilbcps@users.noreply.github.com>
2020-04-20 12:09:57 +0530

    bug fix and full screen added

--- app/src/main/java/com/daytoday/business/dailydelivery/BlankActivity.java
+++ app/src/main/java/com/daytoday/business/dailydelivery/BlankActivity.java
@@ -3,12 +3,14 @@ package com.daytoday.business.dailydelivery;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class BlankActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blank);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
     }
 }
