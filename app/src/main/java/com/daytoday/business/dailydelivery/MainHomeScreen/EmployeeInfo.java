3cb71584ee3f948c0f8e9b5e634b3302b7692d0a
Shuvam Kumar <shuvam1982000@gmail.com>
2020-05-04 22:41:19 +0530

    My employees List

---
+++ app/src/main/java/com/daytoday/business/dailydelivery/MainHomeScreen/EmployeeInfo.java
@@ -0,0 +1,47 @@
+package com.daytoday.business.dailydelivery.MainHomeScreen;
+
+public class EmployeeInfo {
+    String name;
+    String id;
+    String phoneNo;
+    String photourl;
+
+    public EmployeeInfo(String name, String id, String phoneNo, String photourl) {
+        this.name = name;
+        this.id = id;
+        this.phoneNo = phoneNo;
+        this.photourl = photourl;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    public String getPhotourl() {
+        return photourl;
+    }
+
+    public void setPhotourl(String photourl) {
+        this.photourl = photourl;
+    }
+}
