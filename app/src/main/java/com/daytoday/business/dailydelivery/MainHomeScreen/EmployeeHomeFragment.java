a62cad52536c6961038e2304a9859860b93be85a
Shuvam Kumar <shuvam1982000@gmail.com>
2020-05-04 22:41:19 +0530

    My employees List

--- app/src/main/java/com/daytoday/business/dailydelivery/MainHomeScreen/EmployeeHomeFragment.java
+++ app/src/main/java/com/daytoday/business/dailydelivery/MainHomeScreen/EmployeeHomeFragment.java
@@ -3,6 +3,9 @@ package com.daytoday.business.dailydelivery.MainHomeScreen;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,8 +13,10 @@ import android.view.ViewGroup;
 
 import com.daytoday.business.dailydelivery.R;
 
-public class EmployeeHomeFragment extends Fragment {
+import java.util.List;
 
+public class EmployeeHomeFragment extends Fragment {
+    RecyclerView emp_list;
     public EmployeeHomeFragment() {
         // Required empty public constructor
     }
@@ -24,8 +29,19 @@ public class EmployeeHomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view=inflater.inflate(R.layout.fragment_employee_home, container, false);
+        final View view=inflater.inflate(R.layout.fragment_employee_home, container, false);
+        emp_list = view.findViewById(R.id.emp_list);
+        emp_list.setHasFixedSize(true);
+        emp_list.setLayoutManager(new LinearLayoutManager(getContext()));
+        EmployeeViewModel employeeViewModel = new EmployeeViewModel();
 
+        employeeViewModel.getEmployee().observe(getActivity(), new Observer<List<EmployeeInfo>>() {
+            @Override
+            public void onChanged(List<EmployeeInfo> employeeInfos) {
+                EmployeeAdapter employeeAdapter = new EmployeeAdapter(employeeInfos,view.getContext());
+                emp_list.setAdapter(employeeAdapter);
+            }
+        });
         return view;
     }
 }
