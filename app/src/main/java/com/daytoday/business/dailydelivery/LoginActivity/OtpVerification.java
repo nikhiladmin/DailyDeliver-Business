7f767f48698eed3de6c4328783f481f8aefab3a6
Nikhil Kumar Singh <50423438+nikhilbcps@users.noreply.github.com>
2020-05-10 16:07:23 +0530

    bug fix

--- app/src/main/java/com/daytoday/business/dailydelivery/LoginActivity/OtpVerification.java
+++ app/src/main/java/com/daytoday/business/dailydelivery/LoginActivity/OtpVerification.java
@@ -43,15 +43,8 @@ public class OtpVerification extends AppCompatActivity {
     private String code;
     private String name;
 
+    private int SPLASH_SCREEN_TIME = 10000; /*This is the Splash screen time which is 3 seconds*/
 
-
-import com.daytoday.business.dailydelivery.MainHomeScreen.HomeScreen;
-import com.daytoday.business.dailydelivery.R;
-
-public class OtpVerification extends AppCompatActivity {
-    EditText e1, e2, e3, e4, e5, e6;
-    private static int SPLASH_SCREEN_TIME = 10000; /*This is the Splash screen time which is 3 seconds*/
-    static final String TAG="verification_activity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -228,15 +221,12 @@ public class OtpVerification extends AppCompatActivity {
                     if (otp.length() == 1) {
                         e3.requestFocus();
                     } else if (otp.length() == 0)
-
                        e2.requestFocus();
-
                     break;
                 case R.id.edittext3:
                     if (otp.length() == 1) {
                         e4.requestFocus();
                     } else if (otp.length() == 0)
-
                         e3.requestFocus();
                     break;
                 case R.id.edittext4:
@@ -248,26 +238,8 @@ public class OtpVerification extends AppCompatActivity {
                 case R.id.edittext5:
                     if (otp.length() == 1) {
                         e6.requestFocus();
-                    }else{
-
-
+                    }else {
                         e2.requestFocus();
-                    break;
-                case R.id.edittext4:
-                    if (otp.length() == 1) {
-                        Log.e(TAG, "afterTextChanged: 1");
-                        //Intent intent = new Intent(verification_activity.this, HomeScreen.class);
-                        //startActivity(intent);
-                        //finish();
-                        e5.requestFocus();
-                    } else if (otp.length() == 0)
-                        e3.requestFocus();
-                    break;
-                case R.id.edittext5:
-                    if (otp.length() == 1) {
-                        Log.e(TAG, "afterTextChanged: 3");
-                        e6.requestFocus();
-
                     }
                     break;
                 case R.id.edittext6:
@@ -278,16 +250,11 @@ public class OtpVerification extends AppCompatActivity {
 //                      phoneAuthProvider();
                     }else{
                         e5.requestFocus();
-
                         String tempotp = "1111";
                         Log.e(TAG, "afterTextChanged: "+ e3 );
                         Intent intent = new Intent(OtpVerification.this, HomeScreen.class);
                         startActivity(intent);
                         finish();
-                    } else if (otp.length() == 0){
-                        Log.e(TAG, "afterTextChanged:jdhjs " );
-                        e3.requestFocus();
-
                     }
                     break;
                 default:
