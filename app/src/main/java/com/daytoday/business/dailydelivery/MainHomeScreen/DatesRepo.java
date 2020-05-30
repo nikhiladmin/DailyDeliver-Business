b72121cd8e7fe59ac3b207a94f291a61e47d198c
Shuvam Kumar <shuvam1982000@gmail.com>
2020-05-30 12:52:31 +0530

    imp push on Calender view

--- app/src/main/java/com/daytoday/business/dailydelivery/MainHomeScreen/DatesRepo.java
+++ app/src/main/java/com/daytoday/business/dailydelivery/MainHomeScreen/DatesRepo.java
@@ -20,13 +20,13 @@ import java.util.List;
 public class DatesRepo {
     public MutableLiveData<List<Dates>> requestPendingList(String bussId,String custId) {
         MutableLiveData<List<Dates>> liveData = new MutableLiveData<>();
-        List<Dates> list = new ArrayList<>();
         DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
         reference.child("Buss-Cust-DayWise").child(bussId).child(custId).child("Pending")
                 .addValueEventListener(new ValueEventListener() {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                         Iterator iterator = dataSnapshot.getChildren().iterator();
+                        List<Dates> list = new ArrayList<>();
                         while (iterator.hasNext())
                         {
                             DataSnapshot currentSnapshot = (DataSnapshot)iterator.next();
@@ -54,18 +54,28 @@ public class DatesRepo {
 
     public MutableLiveData<List<Dates>> requestAcceptedList(String bussId,String custId) {
         MutableLiveData<List<Dates>> liveData = new MutableLiveData<>();
-        List<Dates> list = new ArrayList<>();
         DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
         reference.child("Buss-Cust-DayWise").child(bussId).child(custId).child("Accepted")
                 .addValueEventListener(new ValueEventListener() {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                         Iterator iterator = dataSnapshot.getChildren().iterator();
+                        List<Dates> list = new ArrayList<>();
                         while (iterator.hasNext())
                         {
                             DataSnapshot currentSnapshot = (DataSnapshot)iterator.next();
-                            Log.i("msg",currentSnapshot.toString());
+                            Log.i("msgdata",currentSnapshot.child("Year").getValue().toString());
+                            String year = currentSnapshot.child("Year").getValue().toString();
+                            String  mon = currentSnapshot.child("Mon").getValue().toString();
+                            String day = currentSnapshot.child("Day").getValue().toString();
+                            String quantity = currentSnapshot.child("quantity").getValue().toString();
+                            Log.i("msg",year + " " + mon + " " + day + " " + quantity);
+                            if (quantity != null)
+                            {
+                                list.add(new Dates(CalendarDay.from(Integer.parseInt(year),Integer.parseInt(mon),Integer.parseInt(day)),quantity));
+                            }
                         }
+                        liveData.setValue(list);
                     }
 
                     @Override
@@ -73,24 +83,33 @@ public class DatesRepo {
 
                     }
                 });
-        liveData.setValue(list);
         return liveData;
     }
 
     public MutableLiveData<List<Dates>> requestCancelledList(String bussId,String custId) {
         MutableLiveData<List<Dates>> liveData = new MutableLiveData<>();
-        List<Dates> list = new ArrayList<>();
         DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
         reference.child("Buss-Cust-DayWise").child(bussId).child(custId).child("Rejected")
                 .addValueEventListener(new ValueEventListener() {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        List<Dates> list = new ArrayList<>();
                         Iterator iterator = dataSnapshot.getChildren().iterator();
                         while (iterator.hasNext())
                         {
                             DataSnapshot currentSnapshot = (DataSnapshot)iterator.next();
-                            Log.i("msg",currentSnapshot.toString());
+                            Log.i("msgdata",currentSnapshot.child("Year").getValue().toString());
+                            String year = currentSnapshot.child("Year").getValue().toString();
+                            String  mon = currentSnapshot.child("Mon").getValue().toString();
+                            String day = currentSnapshot.child("Day").getValue().toString();
+                            String quantity = currentSnapshot.child("quantity").getValue().toString();
+                            Log.i("msg",year + " " + mon + " " + day + " " + quantity);
+                            if (quantity != null)
+                            {
+                                list.add(new Dates(CalendarDay.from(Integer.parseInt(year),Integer.parseInt(mon),Integer.parseInt(day)),quantity));
+                            }
                         }
+                        liveData.setValue(list);
                     }
 
                     @Override
@@ -98,7 +117,6 @@ public class DatesRepo {
 
                     }
                 });
-        liveData.setValue(list);
         return liveData;
     }
 }
