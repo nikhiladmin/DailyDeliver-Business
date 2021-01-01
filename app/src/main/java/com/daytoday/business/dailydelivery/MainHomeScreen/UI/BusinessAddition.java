3479e717620ed7c5c6af6f392508c1ef19f4f93a
divyancod <dysu135@gmail.com>
2021-01-01 15:25:53 +0530

    cropping library included uploading coordination done

--- app/src/main/java/com/daytoday/business/dailydelivery/MainHomeScreen/UI/BusinessAddition.java
+++ app/src/main/java/com/daytoday/business/dailydelivery/MainHomeScreen/UI/BusinessAddition.java
@@ -7,9 +7,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.content.ContextCompat;
 
+import android.app.ProgressDialog;
 import android.app.TimePickerDialog;
+import android.content.Intent;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.text.Html;
@@ -39,7 +42,10 @@ import com.daytoday.business.dailydelivery.Network.Client;
 import com.daytoday.business.dailydelivery.Network.Response.YesNoResponse;
 import com.daytoday.business.dailydelivery.R;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.FirebaseAuth;
@@ -48,14 +54,20 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.squareup.picasso.Picasso;
+import com.yalantis.ucrop.UCrop;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.File;
 import java.util.Calendar;
 import java.util.HashMap;
 
+import de.hdodenhof.circleimageview.CircleImageView;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -65,11 +77,13 @@ public class BusinessAddition extends AppCompatActivity {
     Toolbar toolbar;
     RadioGroup rg1, rg2;
     TextInputLayout textInputLayout;
-    TextInputEditText buisnessName,price;
+    TextInputEditText buisnessName, price;
     ApiInterface apiInterface;
-    String monthOrDay,pay_mode;
-    private ImageView productImg;
-    private final String URL="https://api.icons8.com/api/iconsets/search?term=milk&amount=1";
+    String monthOrDay, pay_mode;
+    CircleImageView profilepic;
+    FloatingActionButton profilePicUploadbtn;
+    private final String URL = "https://api.icons8.com/api/iconsets/search?term=milk&amount=1";
+
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -80,6 +94,10 @@ public class BusinessAddition extends AppCompatActivity {
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         textInputLayout = findViewById(R.id.textInputLayout);
         apiInterface = Client.getClient().create(ApiInterface.class);
+
+        storage = FirebaseStorage.getInstance();
+        storageRef = storage.getReference();
+
         //------------------changing colour of status bar through java startsss----------------------- //
         Window window = this.getWindow();
         window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
@@ -91,15 +109,9 @@ public class BusinessAddition extends AppCompatActivity {
         rg1 = findViewById(R.id.radioGroup1);
         rg1.clearCheck();
         rg2 = findViewById(R.id.radioGroup2);
+        profilePicUploadbtn = findViewById(R.id.profilepiceditbtn);
+        profilepic = findViewById(R.id.business_profile_pic);
         rg2.clearCheck();
-
-        productImg=findViewById(R.id.productImg);
-        //-----------------------Example of Render Image through URL-------------------
-        Picasso.get()
-                .load("https://img.icons8.com/color/10x/milk.png")
-                .resize(5000,5000)
-                .centerCrop()
-                .into(productImg);
 
         //---------------------listener on radio group-----------------------------------------------//
         rg1.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
@@ -131,6 +143,11 @@ public class BusinessAddition extends AppCompatActivity {
                 }
             }
         });
+        profilePicUploadbtn.setOnClickListener(v -> {
+            Intent intent = new Intent();
+            intent.setAction(Intent.ACTION_GET_CONTENT).setType("image/*");
+            startActivityForResult(intent, 1);
+        });
     }
 
     //----------------------------------when the back button of toolbar is pressed--------------------------
@@ -154,22 +171,68 @@ public class BusinessAddition extends AppCompatActivity {
         switch (item.getItemId()) {
             case R.id.add_business:
                 //Toast.makeText(this, "To be Added soon", Toast.LENGTH_SHORT).show();
-                CreateInstanseInFirestore();
+                CreateInstanseInDatabase();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
     }
 
-    private void CreateInstanseInFirestore() {
+    private FirebaseStorage storage;
+    private StorageReference storageRef;
+    String uploadedPicName;
+
+    private void CreateInstanseInDatabase() {
+        ProgressDialog progressDialog =new ProgressDialog(BusinessAddition.this);
+        progressDialog.setCancelable(false);
+        progressDialog.setTitle("Please Wait...");
+        progressDialog.show();
+        uploadedPicName = "pic" + System.currentTimeMillis();
+        StorageReference reference = storageRef.child("BusinessUser/" + uploadedPicName);
+        if (picture != null) {
+            reference.putFile(picture).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    reference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            addNewBusiness(uri.toString());
+                            progressDialog.dismiss();
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(BusinessAddition.this, "Image Upload Failed", Toast.LENGTH_SHORT).show();
+                            addNewBusiness(null);
+                            progressDialog.dismiss();
+                        }
+                    });
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(BusinessAddition.this, "Image Upload Failed", Toast.LENGTH_SHORT).show();
+                    addNewBusiness(null);
+                    progressDialog.dismiss();
+                }
+            });
+        }else
+        {
+            addNewBusiness(null);
+            progressDialog.dismiss();
+        }
+
+    }
+
+    private void addNewBusiness(String imageUrl) {
         FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
         String bussname = buisnessName.getText().toString();
         String priceofbuss = price.getText().toString();
-        Call<YesNoResponse> addbusscall = apiInterface.addBussDetails(bussname,monthOrDay,pay_mode,priceofbuss,currentUser.getUid());
+        Call<YesNoResponse> addbusscall = apiInterface.addBussDetails(bussname, monthOrDay, pay_mode, priceofbuss, currentUser.getUid(), imageUrl);
         addbusscall.enqueue(new Callback<YesNoResponse>() {
             @Override
             public void onResponse(Call<YesNoResponse> call, Response<YesNoResponse> response) {
-                Log.i("message","Response successful " + response.body().getMessage());
+                Log.i("message", "Response successful " + response.body().getMessage());
                 finish();
             }
 
@@ -178,6 +241,40 @@ public class BusinessAddition extends AppCompatActivity {
                 Toast.makeText(BusinessAddition.this, "Some Error Occurred", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    Uri picture, imageUri;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            imageUri = data.getData();
+            startCrop(imageUri);
+        } else if (requestCode == UCrop.REQUEST_CROP && resultCode == RESULT_OK) {
+            Uri uri = UCrop.getOutput(data);
+            profilepic.setImageURI(uri);
+        }
+    }
+
+    private void startCrop(@NonNull Uri uri) {
+        String des = "pic" + System.currentTimeMillis() + ".jpg";
+        UCrop uCrop = UCrop.of(uri, Uri.fromFile(new File(getCacheDir(), des)));
+        uCrop.withAspectRatio(1, 1);
+        uCrop.withMaxResultSize(450, 450);
+        uCrop.withOptions(getCropOptions());
+        uCrop.start(BusinessAddition.this);
+        picture = Uri.fromFile(new File(getCacheDir(), des));
+    }
+
+    private UCrop.Options getCropOptions() {
+        UCrop.Options options = new UCrop.Options();
+        options.setHideBottomControls(false);
+        options.setFreeStyleCropEnabled(true);
+        options.setCompressionQuality(50);
+        options.setToolbarTitle("Crop Your Image");
+        options.setStatusBarColor(getResources().getColor(R.color.colorPrimary));
+        return options;
     }
 
 }
--- app/src/main/java/com/daytoday/business/dailydelivery/MainHomeScreen/UI/BusinessDetailActivity.java
+++ app/src/main/java/com/daytoday/business/dailydelivery/MainHomeScreen/UI/BusinessDetailActivity.java
@@ -1,6 +1,9 @@
 package com.daytoday.business.dailydelivery.MainHomeScreen.UI;
 
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -25,16 +28,23 @@ import com.daytoday.business.dailydelivery.Utilities.AppConstants;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.squareup.picasso.Picasso;
+import com.yalantis.ucrop.UCrop;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 
+import de.hdodenhof.circleimageview.CircleImageView;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -46,52 +56,57 @@ public class BusinessDetailActivity extends AppCompatActivity {
     Button button;
     Bussiness bussiness;
     String paymode, mord;
-    ImageView bussImg;
+    CircleImageView businessImage;
+    FloatingActionButton imageEditButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buisness_detail);
         bussiness = (Bussiness) getIntent().getSerializableExtra("buisness-object");
-        getSupportActionBar().setTitle(bussiness.getName()+" - Details");
+        getSupportActionBar().setTitle(bussiness.getName() + " - Details");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getWindow().setStatusBarColor(getResources().getColor(R.color.colorPrimaryDark));
 
-        Log.e("TAG", "onCreate: "+FirebaseAuth.getInstance().getCurrentUser().getDisplayName());
+        Log.e("TAG", "onCreate: " + FirebaseAuth.getInstance().getCurrentUser().getDisplayName());
         rg1 = findViewById(R.id.radioGroup1);
         rg2 = findViewById(R.id.radioGroup2);
         buss_name = findViewById(R.id.buss_name);
         buss_price = findViewById(R.id.buss_price);
         buss_address = findViewById(R.id.buss_address);
-        bussImg = findViewById(R.id.BusinessImg);
         button = findViewById(R.id.submitbutton);
-        setFalse();
+        businessImage = findViewById(R.id.business_profile_pic);
+        imageEditButton = findViewById(R.id.profilepiceditbtn);
         buss_name.setText(bussiness.getName());
         buss_price.setText("" + bussiness.getPrice());
-        Picasso.get()
-                .load(bussiness.getImgurl())
-                .resize(5000,5000)
-                .centerCrop().into(bussImg);
+        if (bussiness.getImgurl() != null) {
+            Picasso.get()
+                    .load(bussiness.getImgurl())
+                    .resize(5000, 5000)
+                    .centerCrop().into(businessImage);
+        }
         Log.e("TAG", "onCreate: " + bussiness.getdOrM());
         buss_address.setText(bussiness.getAddress());
         if (bussiness.getdOrM().equals("D")) {
             rg1.check(R.id.radio_btn_daily);
-            mord="Daily";
+            mord = "Daily";
         } else {
             rg1.check(R.id.radio_btn_monthly);
-            mord="Monthly";
+            mord = "Monthly";
         }
 
         if (bussiness.getPayment().equals("Cash")) {
             rg2.check(R.id.radio_btn_cash);
-            paymode="Cash";
+            paymode = "Cash";
         } else if (bussiness.getPayment().equals("Online")) {
             rg2.check(R.id.radio_btn_online);
-            paymode="Online";
+            paymode = "Online";
         } else {
-            paymode="Both";
+            paymode = "Both";
             rg2.check(R.id.radio_btn_both);
         }
+        storage = FirebaseStorage.getInstance();
+        storageRef = storage.getReference();
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -101,18 +116,23 @@ public class BusinessDetailActivity extends AppCompatActivity {
                 alertDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        updateData();
+                        uploadImage();
                     }
                 });
                 alertDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        Snackbar.make(v,"Data Update Cancelled",Snackbar.LENGTH_SHORT).show();
+                        Snackbar.make(v, "Data Update Cancelled", Snackbar.LENGTH_SHORT).show();
                     }
                 }).show();
             }
         });
-
+        imageEditButton.setOnClickListener(v -> {
+            Intent intent = new Intent();
+            intent.setAction(Intent.ACTION_GET_CONTENT).setType("image/*");
+            startActivityForResult(intent, 1);
+        });
+        setFalse();
     }
 
     @Override
@@ -137,6 +157,7 @@ public class BusinessDetailActivity extends AppCompatActivity {
                 buss_name.setEnabled(true);
                 buss_price.setEnabled(true);
                 buss_address.setEnabled(true);
+                imageEditButton.setClickable(true);
                 for (int i = 0; i < rg1.getChildCount(); i++)
                     rg1.getChildAt(i).setEnabled(true);
                 for (int i = 0; i < rg2.getChildCount(); i++)
@@ -147,9 +168,9 @@ public class BusinessDetailActivity extends AppCompatActivity {
     }
 
 
-    public void updateData() {
+    public void updateData(String imageurl) {
         String name = buss_name.getText().toString().trim();
-        String adress  = buss_address.getText().toString().trim();
+        String adress = buss_address.getText().toString().trim();
         rg1.setOnCheckedChangeListener((group, checkedId) -> {
             RadioButton radioButton = (RadioButton) group.findViewById(checkedId);
             if (null != radioButton) {
@@ -163,54 +184,112 @@ public class BusinessDetailActivity extends AppCompatActivity {
             }
         });
         String price = buss_price.getText().toString().trim();
-        /*
-        FirebaseFirestore firestore = FirebaseFirestore.getInstance();
-        DocumentReference documentReference = firestore.collection("Buss_Info").document("" + bussiness.getBussid());
-        Log.e("TAG", "updateData: " + paymode + mord);
-        documentReference.update(updates)
-                .addOnSuccessListener(new OnSuccessListener<Void>() {
-                    @Override
-                    public void onSuccess(Void aVoid) {
-                        Log.d("TAG", "DocumentSnapshot successfully updated!");
-                        Snackbar.make(findViewById(android.R.id.content),"Data Updated Successfully",Snackbar.LENGTH_LONG).show();
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.w("TAG", "Error updating document", e);
-                        Snackbar.make(findViewById(android.R.id.content),"Data update failed. Try Again",Snackbar.LENGTH_LONG).show();
-                    }
-                });
-        */
         ApiInterface apiInterface = Client.getClient().create(ApiInterface.class);
-        Call<YesNoResponse> updateBussDetailsCall = apiInterface.updateBussDetails(bussiness.getBussid(),name,"",adress,price,paymode);
+        Call<YesNoResponse> updateBussDetailsCall = apiInterface.updateBussDetails(bussiness.getBussid(), name, "", adress, price, paymode, imageurl);
         updateBussDetailsCall.enqueue(new Callback<YesNoResponse>() {
             @Override
             public void onResponse(Call<YesNoResponse> call, Response<YesNoResponse> response) {
-                if (!response.body().getError()){
+                if (!response.body().getError()) {
                     Toast.makeText(BusinessDetailActivity.this, "Updated Successfully", Toast.LENGTH_SHORT).show();
                 }
             }
 
             @Override
             public void onFailure(Call<YesNoResponse> call, Throwable t) {
-                Log.i(AppConstants.ERROR_LOG,"Some Error Occurred in BusinessDetailActivity Error is : { " + t.getMessage() + " }");
+                Log.i(AppConstants.ERROR_LOG, "Some Error Occurred in BusinessDetailActivity Error is : { " + t.getMessage() + " }");
             }
         });
         setFalse();
     }
 
-    public void setFalse()
-    {
+    public void setFalse() {
         button.setEnabled(false);
         buss_name.setEnabled(false);
         buss_price.setEnabled(false);
         buss_address.setEnabled(false);
+        imageEditButton.setClickable(false);
         for (int i = 0; i < rg1.getChildCount(); i++)
             rg1.getChildAt(i).setEnabled(false);
         for (int i = 0; i < rg2.getChildCount(); i++)
             rg2.getChildAt(i).setEnabled(false);
     }
 
+    Uri picture, imageUri;
+    private FirebaseStorage storage;
+    private StorageReference storageRef;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            imageUri = data.getData();
+            startCrop(imageUri);
+        } else if (requestCode == UCrop.REQUEST_CROP && resultCode == RESULT_OK) {
+            Uri uri = UCrop.getOutput(data);
+            businessImage.setImageURI(uri);
+        }
+    }
+
+    private void startCrop(@NonNull Uri uri) {
+        String des = "pic" + System.currentTimeMillis() + ".jpg";
+        UCrop uCrop = UCrop.of(uri, Uri.fromFile(new File(getCacheDir(), des)));
+        uCrop.withAspectRatio(1, 1);
+        uCrop.withMaxResultSize(450, 450);
+        uCrop.withOptions(getCropOptions());
+        uCrop.start(BusinessDetailActivity.this);
+        picture = Uri.fromFile(new File(getCacheDir(), des));
+    }
+
+    private UCrop.Options getCropOptions() {
+        UCrop.Options options = new UCrop.Options();
+        options.setHideBottomControls(false);
+        options.setFreeStyleCropEnabled(true);
+        options.setCompressionQuality(50);
+        options.setToolbarTitle("Crop Your Image");
+        options.setStatusBarColor(getResources().getColor(R.color.colorPrimary));
+        return options;
+    }
+
+    String uploadedPicName;
+
+    private void uploadImage() {
+        ProgressDialog progressDialog = new ProgressDialog(BusinessDetailActivity.this);
+        progressDialog.setCancelable(false);
+        progressDialog.setTitle("Please Wait...");
+        progressDialog.show();
+        uploadedPicName = "pic" + System.currentTimeMillis();
+        StorageReference reference = storageRef.child("BusinessUser/" + uploadedPicName);
+        if (picture != null) {
+            reference.putFile(picture).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    reference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            updateData(uri.toString());
+                            progressDialog.dismiss();
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(BusinessDetailActivity.this, "Image Upload Failed", Toast.LENGTH_SHORT).show();
+                            updateData(null);
+                            progressDialog.dismiss();
+                        }
+                    });
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(BusinessDetailActivity.this, "Image Upload Failed", Toast.LENGTH_SHORT).show();
+                    updateData(null);
+                    progressDialog.dismiss();
+                }
+            });
+        } else {
+            updateData(null);
+            progressDialog.dismiss();
+        }
+
+    }
 }
